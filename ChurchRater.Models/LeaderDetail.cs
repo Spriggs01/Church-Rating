ee16adc333b5d53bb7fdbfb5d2b18579005d456b
wtravis923 <willie.travis@gmail.com>
2019-05-02 10:23:03 -0400

    test

---
+++ ChurchRater.Models/LeaderDetail.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChurchRater.Models.LeaderModels
+{
+    public class LeaderDetail
+    {
+        public int ChurchID { get; set; }
+
+        public Guid UserID { get; set; }
+
+        public string ChurchName { get; set; }
+
+        public string ChurchLocation { get; set; }
+
+        public string CommunityInvolvement { get; set; }
+
+        public string Friendliness { get; set; }
+
+        public string Facilities { get; set; }
+
+        public string Music { get; set; }
+
+        public string Message { get; set; }
+    }
+}
---
+++ ChurchRater.Models/LeaderRatingModels/LeaderRatingEdit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChurchRater.Models.LeaderRatingModels
+{
+    public class LeaderRatingEdit
+    {
+        public int LeaderID { get; set; }
+        public int SpeakingAbilityRating { get; set; }
+
+        public int EngaginRating { get; set; }
+
+        public int AuthenticRating { get; set; }
+
+        public int RapportRating { get; set; }
+    }
+}
